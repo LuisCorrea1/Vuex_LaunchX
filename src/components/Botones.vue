e90a96b042cee92ccb9780c47267a9170e21110b
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-03-30 19:45:40 -0600

    First commit

---
+++ src/components/Botones.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="buttons">
+      <!--Para mutaciones se usa commit para mandar llamar la información-->
+      <button @click="$store.dispatch('bajarContador')">-</button>
+      <button @click="$store.dispatch('subirContador')">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Botones',
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,41 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  //Informacion que se muestra o maneja dentro de la App (Variables,cambios de informacion, etc)
   state: {
+    contador: 0
   },
+  //Metodos de entrada para recuperar la información del state
   getters: {
+    cuadrado(state){
+      return state.contador * state.contador;
+    }
   },
-  mutations: {
+  //Para hacer modificaciones dentro de la información del estado
+  mutations: { //Usar commit para llamar informacion
+    subirContador(state, random) {
+      state.contador += random;
+    },
+    bajarContador(state,random) {
+      state.contador -=random;
+    }
   },
-  actions: {
+  //Misma función que las mutaciones, solo que las acciones si pueden tener código async (Ej. Recuperar info de un API)
+  actions: { //usar dispatch para llamar informacion 
+    async subirContador({ commit }) {
+      const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new');
+      const results = await res.json();
+      console.log(results);
+      commit("subirContador",results)
+    },
+    async bajarContador({ commit }) {
+      const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new');
+      const results = await res.json();
+      console.log(results);
+      commit("bajarContador",results)
+    }
   },
+  //Condicionales de navegación
   modules: {
   }
 })
